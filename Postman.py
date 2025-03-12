19afec430fddd551a411de0d1d9474146bf0a2cb
Taleb Choaib <choaib03dz@gmail.com>
2025-03-12 06:56:04 +0300

    Initial commit

---
+++ Postman.py
@@ -0,0 +1,17 @@
+{
+  "events": [
+    {
+      "type": "message",
+      "replyToken": "REPLY_TOKEN",
+      "source": {
+        "userId": "Ua673da6876bab906ce8734e94e59502a",
+        "type": "user"
+      },
+      "message": {
+        "id": "MESSAGE_ID",
+        "text": ".r",
+        "type": "text"
+      }
+    }
+  ]
+}
